2f892d404ede520c5972e37ef9f6e6c19f6024d1
Benedict Forrest <benedict.forrest@me.com>
2021-03-31 11:50:18 +1100

    style "Alert" component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,25 +5,22 @@ import useNotifications from "@elevio/kb-kit/lib/hooks/useNotifications";
 const Alert = ({ className }) => {
   const { notification, clearNotification } = useNotifications();
   if (!notification) return null;
-
-  const colour = getNotificationColour(notification.type);
+  const colourClass = getNotificationColour(notification.type);
   return (
     <div
       role="alert"
-      className={cx(
-        "flex items-center text-white px-4 py-3 relative",
-        className,
-        colour
-      )}
+      className={cx("alert", className, colourClass )}
     >
-      <span className="block sm:inline" data-testid="alert-message">
+      <div className="alert-message" data-testid="alert-message">
         {notification.message}
-      </span>
+      </div>
       <button
-        className="absolute top-0 bottom-0 right-0 px-4 py-3"
+        className="alert-close"
         onClick={clearNotification}
       >
-        {/* <Icon icon="cross" className="fill-current h-6 w-6 text-white" /> */}
+        <svg width="24" height="24" viewBox="0 0 24 24" className="alert-icon">
+          <path d="M18.7071 6.70711C19.0976 6.31658 19.0976 5.68342 18.7071 5.29289C18.3166 4.90237 17.6834 4.90237 17.2929 5.29289L12 10.5858L6.70711 5.29289C6.31658 4.90237 5.68342 4.90237 5.29289 5.29289C4.90237 5.68342 4.90237 6.31658 5.29289 6.70711L10.5858 12L5.29289 17.2929C4.90237 17.6834 4.90237 18.3166 5.29289 18.7071C5.68342 19.0976 6.31658 19.0976 6.70711 18.7071L12 13.4142L17.2929 18.7071C17.6834 19.0976 18.3166 19.0976 18.7071 18.7071C19.0976 18.3166 19.0976 17.6834 18.7071 17.2929L13.4142 12L18.7071 6.70711Z" />
+        </svg>
       </button>
     </div>
   );
@@ -34,9 +31,9 @@ export default Alert;
 const getNotificationColour = (type) => {
   switch (type) {
     case "success":
-      return "bg-success";
+      return "alert-success";
     case "error":
     default:
-      return "bg-danger";
+      return "alert-danger";
   }
 };
